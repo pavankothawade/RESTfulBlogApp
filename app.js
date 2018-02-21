7c0f880529b60aa94e8cc601ef4c66d0f6469a69
pavankothawade <pavan.kothawade@sjsu.edu>
2018-02-21 11:50:51 -0800

    mongoose connection

--- app.js
+++ app.js
@@ -15,8 +15,21 @@ var blogSchema= new mongoose.Schema({
 });
 
 var Blog= mongoose.model("Blog", blogSchema);
+
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index", {blogs: blogs});		
+		}
+	});
+	
+});
+
 app.get("/", function(req, res){
-	res.send("This is our homepage");
+	res.redirect("/blogs");
 });
 app.listen(3000, function(){
    console.log("The YelpCamp Server Has Started!");
